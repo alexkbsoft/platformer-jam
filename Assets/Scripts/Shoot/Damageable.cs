9d38d3c5aeba2d8b63438c44e0ad2feea310d545
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-05-10 13:39:15 +0500

    fixed bullet

--- Assets/Scripts/Shoot/Damageable.cs
+++ Assets/Scripts/Shoot/Damageable.cs
@@ -1,16 +1,20 @@
-﻿using UnityEngine;
+﻿using Health;
+using UnityEngine;
 
 namespace Shoot
 {
     public class Damageable : MonoBehaviour
     {
         [SerializeField] private float damage;
-        
-        private void OnCollisionEnter2D(Collision2D other) => DoDamage();
 
-        protected virtual void DoDamage()
+        private void OnCollisionEnter2D(Collision2D other)
         {
-            Debug.Log($"Нанесено {damage} урона");
+            DoDamage(other.gameObject);
+        }
+
+        protected virtual void DoDamage(GameObject go)
+        {
+            if (go.gameObject.TryGetComponent(out HealthProcessor health)) health.TakeDamage(damage);
         }
     }
 }
